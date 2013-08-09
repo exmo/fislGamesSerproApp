9453066abf10ae8b2e0213c3faf9a437701422b3
Thiago Mariano <mariano.thiago@gmail.com>
2013-08-09 16:56:19 -0300

    mudança do pacote de br.gov.serpro.fisl para br.gov.serpro.gamelivre

--- fislGamesSerproApp/src/br/gov/serpro/gamelivre/FislActivity.java
+++ fislGamesSerproApp/src/br/gov/serpro/gamelivre/FislActivity.java
@@ -7,7 +7,9 @@ public class FislActivity extends DroidGap {
  
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
        super.onCreate(savedInstanceState);
+       
        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
